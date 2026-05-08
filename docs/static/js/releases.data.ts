08b1fccacac31219cd30569c125c8a2ac6c60ee1
Sjj1024 <15670339118@163.com>
2026-05-08 18:36:19 +0800

    Refactor release data fetching in releases.data.ts to include error handling and fallback URL. Enhance logging for better debugging of data retrieval from both primary and secondary endpoints.

--- docs/static/js/releases.data.ts
+++ docs/static/js/releases.data.ts
@@ -1,11 +1,21 @@
 export default {
     // get data from local
     async load() {
-        const getReleases = await fetch(
-            'https://server.pakeplus.com/public/latest/version'
-        )
-        const data = await getReleases.json()
-        console.log('getReleases data', JSON.stringify(data))
-        return data
+        try {
+            const getReleases = await fetch(
+                'https://server.pakeplus.com/public/latest/version'
+            )
+            const data = await getReleases.json()
+            console.log('pakeplus.com data', JSON.stringify(data[0]))
+            return data
+        } catch (error) {
+            console.error('pakeplus.com error', error)
+            const getReleases = await fetch(
+                'https://pakeplus.top/api/public/latest/version'
+            )
+            const data = await getReleases.json()
+            console.log('pakeplus.top data', JSON.stringify(data[0]))
+            return data
+        }
     },
 }
